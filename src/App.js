4990d8a2a576243627135a4d5b067c916e5df6ed
Stephanie Anderson <srglover126@gmail.com>
2025-04-09 11:02:22 -0400

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,44 +1,51 @@
 import React from "react";
 import Weather from "./Weather";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css"; //remove?
+import "bootstrap/dist/js/bootstrap.bundle.min.js"; //remove?
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>
-          <span className="emoji">✨</span> Welcome to your Weather{" "}
-          <span className="emoji">✨</span>
-        </h1>
+      <div className="container">
+        <header className="App-header">
+          <h1>
+            <span className="emoji">✨</span> Welcome to your Weather{" "}
+            <span className="emoji">✨</span>
+          </h1>
 
-        <Weather />
-      </header>
-      <footer>
-        This project was coded by
-        <a
-          href="https://www.linkedin.com/in/stephanie-anderson-rn-bsn-4658b336/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          {""} Stephanie Anderson
-        </a>
-        , is open sourced on
-        <a
-          href="https://github.com/IslaAdventura/weather-react"
-          target="_blank"
-          rel="noreferrer"
-        >
-          {""} GitHub
-        </a>
-        , and hosted on
-        <a
-          href="https://resplendent-caramel-d48712.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          {""} Netlify
-        </a>
-      </footer>
+          <Weather />
+        </header>
+        <footer>
+          <button type="button" className="btn">
+            Base class
+          </button>
+          <span className="emoji">✨</span>This project was coded by
+          <a
+            href="https://www.linkedin.com/in/stephanie-anderson-rn-bsn-4658b336/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {""} Stephanie Anderson
+          </a>
+          , is open sourced on
+          <a
+            href="https://github.com/IslaAdventura/weather-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {""} GitHub
+          </a>
+          , and hosted on
+          <a
+            href="https://resplendent-caramel-d48712.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {""} Netlify<span className="emoji">✨</span>
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
