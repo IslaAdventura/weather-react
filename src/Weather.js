38bb313eccf7c82837120786647dfa946584ff54
Stephanie Anderson <srglover126@gmail.com>
2025-04-09 15:05:42 -0400

    fixed error messages

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import axios from "axios";
+import React from "react";
+//import axios from "axios";
 //import PacmanLoader from "react-spinners/PacmanLoader";
 import "./Weather.css";
 
