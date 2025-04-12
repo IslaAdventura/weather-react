1f2c695d4b84e084224d816b7d6c7761193fb501
Stephanie Anderson <srglover126@gmail.com>
2025-04-11 22:41:46 -0400

    added animated weather icon

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
+import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherInfo(props) {
   function formatTime(date) {
@@ -51,14 +52,14 @@ export default function WeatherInfo(props) {
         </li>
         <li className="text-capitalize">{props.data.description}</li>
       </ul>
+
       <div className="row mt-3">
         <div className="col-6">
           <div className="clearfix">
-            <img
-              src={props.data.iconUrl}
-              alt={props.data.description}
-              className="float-left"
-            />
+            <div className="float-left">
+              <WeatherIcon code={props.data.icon} />
+            </div>
+
             <div className="float-left">
               <span className="temperature-number">
                 {Math.round(props.data.temperature)}
@@ -67,6 +68,7 @@ export default function WeatherInfo(props) {
             </div>
           </div>
         </div>
+
         <div className="col-6">
           <ul>
             <li>Precipitation: ****15%****</li>
